e8632feef842f394af2c3c6d4fa1c37c89b38726
gvarys <johnnyzaev@gmail.com>
2022-11-28 15:19:02 +0600

    Simple movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 namespace Player
 {
+	[RequireComponent(typeof(CharacterController))]
 	public class PlayerController : MonoBehaviour
 	{
+		private CharacterController _controller;
+
+		#region Variables: Movement
+
+		private const float MoveSpeed = 5f;
+		private Vector3 _move;
+
+		#endregion
 		private void Start()
 		{
-        
+			_controller = GetComponent<CharacterController>();
+			_move = new Vector3();
 		}
 
 		private void Update()
 		{
-        
+			_move.x = Input.GetAxisRaw("Horizontal");
+			_move.z = Input.GetAxisRaw("Vertical");
+			_controller.Move(Time.deltaTime * MoveSpeed * _move);
 		}
 	}
 }
